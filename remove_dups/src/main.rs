9ba13768536c31d141d485c7142e49519155a917
GimpFlamingo <rayjkochenderfer@gmail.com>
2020-07-03 09:16:28 -0400

    Finish building linked list

--- remove_dups/src/main.rs
+++ remove_dups/src/main.rs
@@ -1,5 +1,6 @@
 use std::rc::Rc;
 
+// Singly linked list specifically
 pub struct List<T> {
     head: Link<T>,
 }
@@ -14,6 +15,60 @@ struct Node<T> {
 impl<T> List<T> {
     pub fn new() -> Self {
         List { head: None }
+    }
+
+    /// Adds a node to the end of the list
+    pub fn append(&self, elem: T) -> List<T> {
+        List {
+            head: Some(Rc::new(Node {
+                elem: elem,
+                next: self.head.clone(),
+            }))
+        }
+    }
+
+    pub fn tail(&self) -> List<T> {
+        List {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.elem)
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop (&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.head.as_ref().map(|node| &**node) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.elem
+        })
     }
 }
 
